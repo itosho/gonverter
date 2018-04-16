bbe9d4194d07c57a251726b3ae92ef35595d286b
itosho <sho.ito@supership.jp>
2018-04-16 23:16:37 +0900

    fix function

--- image_type.go
+++ image_type.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"io"
+	"os"
+	"path/filepath"
 )
 
 type imageType interface {
@@ -20,4 +23,44 @@ func getImageType(ext string) imageType {
 	default:
 		return nil
 	}
+}
+
+func filePathWalk(directory string, fromType imageType, toType imageType) int {
+	err := filepath.Walk(directory, func(path string, info os.FileInfo, err error) error {
+		fromExt := fromType.getExt()
+
+		if filepath.Ext(path) == fromExt {
+			file, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			defer file.Close()
+
+			img, err := fromType.decode(file)
+			if err != nil {
+				return err
+			}
+
+			out, err := os.Create(path[:len(path)-len(fromExt)] + toType.getExt())
+			if err != nil {
+				return err
+			}
+			defer out.Close()
+
+			toType.encode(out, img)
+
+			if err := os.Remove(path); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return ExitError
+	}
+
+	return ExitSuccess
 }
--- main.go
+++ main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"os"
-	"path/filepath"
 )
 
 const (
@@ -39,44 +37,4 @@ func main() {
 
 	code := filePathWalk(directory, fromType, toType)
 	os.Exit(code)
-}
-
-func filePathWalk(directory string, fromType imageType, toType imageType) int {
-	err := filepath.Walk(directory, func(path string, info os.FileInfo, err error) error {
-		fromExt := fromType.getExt()
-
-		if filepath.Ext(path) == fromExt {
-			file, err := os.Open(path)
-			if err != nil {
-				return err
-			}
-			defer file.Close()
-
-			img, err := fromType.decode(file)
-			if err != nil {
-				return err
-			}
-
-			out, err := os.Create(path[:len(path)-len(fromExt)] + toType.getExt())
-			if err != nil {
-				return err
-			}
-			defer out.Close()
-
-			toType.encode(out, img)
-
-			if err := os.Remove(path); err != nil {
-				return err
-			}
-		}
-
-		return nil
-	})
-
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		return ExitError
-	}
-
-	return ExitSuccess
 }
